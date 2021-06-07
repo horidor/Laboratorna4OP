da41c3f5a5fb811e459d942bd4e863cbcb81d87b
Maxim <makson202007@gmail.com>
2021-06-07 21:05:03 +0300

    Finalized code, removed test bmp files

--- Laboratorna4OP/main_g.cpp
+++ Laboratorna4OP/main_g.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <fstream>
 
-typedef struct {
+typedef struct { //заголовок файлу
     int8_t id[2];            // Завжди дві літери 'B' і 'M'
     int32_t filesize;        // Розмір файла в байтах
     int16_t reserved[2];     // 0, 0
@@ -20,13 +20,13 @@ typedef struct {
     int32_t biClrImportant;  // Те саме
 } BMPHEAD;
 
-typedef struct {
+typedef struct { //піксель
     int8_t redC;
     int8_t greenC;
     int8_t blueC;
 } PIXELDATA;
 
-class BITMAP
+class BITMAP //клас бітмапу, реалізує збереження його даних, збільшення у декілка разів та вивод у файл формату .bmp
 {
 private:
     BMPHEAD bh;
@@ -43,16 +43,11 @@ private:
         return (int16_t*)&a;
     }
 
-    void create_header(unsigned char* HEAD)
+    void download_header(unsigned char* HEAD) //завантаження заголовку з файла
     {
-        int32_t* int32p;
-        int16_t* int16p;
         bh.id[0] = HEAD[0];
         bh.id[1] = HEAD[1];
-        //bh.filesize = *(give_by_address32(HEAD[2]));
-        //int32p = (int32_t*)&HEAD[2];
         bh.filesize = *(give_by_address32(HEAD[2]));
-        //std::cout << bh.filesize;
         for (int i = 0; i < 2; i++)
         {
             bh.reserved[i] = *(give_by_address16(HEAD[6 + i * 2]));
@@ -71,7 +66,7 @@ private:
         bh.biClrImportant = *(give_by_address32(HEAD[50]));
     }
 
-    void create_pixeldata(unsigned char* pixel_bytes)
+    void create_pixeldata(unsigned char* pixel_bytes) //завантаження пікселей у клас
     {
         pixels = new PIXELDATA[bh.width*bh.depth];
         int pointer;
@@ -83,12 +78,11 @@ private:
                 pixels[i*bh.width + j].blueC = pixel_bytes[pointer];
                 pixels[i*bh.width + j].greenC = pixel_bytes[pointer + 1];
                 pixels[i*bh.width + j].redC = pixel_bytes[pointer + 2];
-                //std::cout << pixels[i * bh.depth + j].blueC + 48 << " " << pixels[i * bh.depth + j].greenC + 48 << " " << pixels[i * bh.depth + j].redC + 48 << std::endl;
             }
         }
     }
 
-    void copy_header(BMPHEAD bhc, int mult = 1)
+    void copy_header(BMPHEAD bhc, int mult = 1)//копіювання хедера с наданного бітмапу зі зміною розміра файла та його ширини та висоти
     {
         bh.id[0] = bhc.id[0];
         bh.id[1] = bhc.id[1];
@@ -109,8 +103,9 @@ private:
         bh.biClrImportant = bhc.biClrImportant;
     }
 
-    void copy_resiz_pixeldata(PIXELDATA* pixels_o, int d_old, int w_old)
+    void copy_resiz_pixeldata(PIXELDATA* pixels_o, int d_old, int w_old) //копіювання (та збільшення) розміру пікселей
     {
+        std::cout << "Resizing file by a factor of " << bh.depth / d_old << "... ";
         pixels = new PIXELDATA[bh.width * bh.depth];
         int scale;
         for (int i = 0; i < bh.depth; i++)
@@ -121,11 +116,9 @@ private:
                 pixels[i * bh.width + j].blueC = pixels_o[scale].blueC;
                 pixels[i * bh.width + j].greenC = pixels_o[scale].greenC;
                 pixels[i * bh.width + j].redC = pixels_o[scale].redC;
-                //std::cout << scale << " ";
-                //std::cout << pixels[i*bh.depth + j].blueC + 48 << " " << pixels[i * bh.depth + j].greenC + 48 << " " << pixels[i * bh.depth + j].redC + 48 << std::endl;
-                //std::cout << pixels_o[scale].blueC + 48 << " " << pixels_o[scale].greenC + 48 << " " << pixels_o[scale].redC + 48 << std::endl;
             }
         }
+        std::cout << "Done." << std::endl;
     }
 
     unsigned char* readdress32(int32_t a)
@@ -138,7 +131,7 @@ private:
         return (unsigned char*)&a;
     }
 
-    unsigned char* buffer_header()
+    unsigned char* buffer_header() //покладання заголовку у буфер
     {
         unsigned char* HEAD = new unsigned char[54];
         unsigned char* ch;
@@ -222,7 +215,7 @@ private:
         return HEAD;
     }
 
-    unsigned char* buffer_pixeldata()
+    unsigned char* buffer_pixeldata() //покладання пікселей у буфер
     {
         unsigned char* buf_pixels = new unsigned char[bh.filesize - 54];
         int pointer=0;
@@ -234,7 +227,6 @@ private:
                 buf_pixels[pointer] = pixels[i * bh.width + j].blueC;
                 buf_pixels[pointer + 1] = pixels[i * bh.width + j].greenC;
                 buf_pixels[pointer + 2] = pixels[i * bh.width + j].redC;
-                //std::cout << buf_pixels[pointer]+48 << " " << buf_pixels[pointer+1]+48 << " " << buf_pixels[pointer+2]+48 << std::endl;
                 
             }
             if (padding != 0)
@@ -242,10 +234,8 @@ private:
                 pointer += 3;
                 for (int k = 0; k < padding; k++)
                 {
-                    //std::cout << pointer << std::endl;
                     buf_pixels[pointer] = '\0';
                     pointer++;
-                    //std::cout << padding << std::endl;
                 }
             }
         }
@@ -253,7 +243,7 @@ private:
     }
 
 public:
-    BITMAP(std::string name)
+    BITMAP(std::string name) //перший конструктор - отримує назву файла, конструює об'єкт за інформацією, яка зберігається у файлі
     {
         std::fstream pFile(name, std::ios::in | std::ios::binary);
         if (pFile.fail())
@@ -264,7 +254,7 @@ public:
 
         unsigned char HEAD[54];
         pFile.read((char*)&HEAD, 54);
-        create_header(HEAD);
+        download_header(HEAD);
 
         padding = (4 - (bh.width * 3) % 4) % 4;
 
@@ -276,14 +266,14 @@ public:
         delete[] pixel_byte;
     }
 
-    BITMAP(BITMAP BM, int mult)
+    BITMAP(BITMAP BM, int mult) //другий конструкор - отримує інший бітмап та коефіцієнт збільшення, конструє об'єкт збільшенний на коефіцієнт збільшення
     {
         copy_header(BM.get_header(), mult);
         copy_resiz_pixeldata(BM.get_pixels(), bh.depth / mult, bh.width / mult);
         padding = (4 - (bh.width * 3) % 4) % 4;
     }
 
-    void OUTPUT_BITMAP(std::string name)
+    void OUTPUT_BITMAP(std::string name) //вивод бітмапу у файл за назвою
     {
         std::fstream pFile(name, std::ios::out | std::ios::binary);
         if (pFile.fail())
@@ -299,11 +289,12 @@ public:
         pFile.write((char*)PIXELS, bh.filesize - 54);
 
         pFile.close();
+        std::cout << "Result is saved in " << name << std::endl;
         delete[] HEAD;
         delete[] PIXELS;
     }
 
-    BMPHEAD get_header() { return bh; }
+    BMPHEAD get_header() { return bh; } //геттери
     PIXELDATA* get_pixels() { return pixels; }
 
 };
@@ -311,192 +302,7 @@ public:
 
 int main(int _argc, char* _argv[])
 {
-
-   // //BMPHEAD bh;
-
-
-   // std::fstream pFile("tested.bmp", std::ios::in | std::ios::binary);
-   // unsigned char HEAD[54];
-   // pFile.read((char*)&HEAD, 54);
-   // //char* b;
-   // //int* a;
-   // //b = (char*) &HEAD[22];
-   // //a = (int*) &HEAD[3];
-   // //std::cout << *a;
-
-   // int mult;
-   // int* wid, * height;
-   // wid = (int*)&HEAD[18];
-   // height = (int*)&HEAD[22];
-   // std::cout << *wid << std::endl;
-   // std::cout << *height << std::endl;
-   // mult = 2;
-   // PIXELDATA** pixels = new PIXELDATA * [*height];
-   // for (int i = 0; i < *height; i++)
-   // {
-   //     pixels[i] = new PIXELDATA[*wid];
-   // }
-   // //PIXELDATA pixels[8][8];
-   // int* file_size = (int*)&HEAD[2];
-   // *file_size -= 54;
-
-   // unsigned char* pixels_byte = new unsigned char[*file_size];
-   // //unsigned char pixels_byte[192];
-   // pFile.read((char*)pixels_byte, *file_size);
-   // int pointer;
-   // int padding;
-   // padding = (4 - (*wid*3) % 4) % 4;
-   // for (int i = 0; i < *height; i++)
-   // {
-   //     for (int j = 0; j < *wid; j++)
-   //     {
-   //         pointer = i * (*height)*3 + j * 3 + padding * i;
-   //         pixels[i][j].blueC = pixels_byte[pointer];
-   //         pixels[i][j].greenC = pixels_byte[pointer + 1];
-   //         pixels[i][j].redC = pixels_byte[pointer + 2];
-   //     }
-   // }
-
-   // int widn;
-   // int heightn;
-   // int file_sizen;
-
-   // pFile.close();
-
-   // widn = (*wid) * mult;
-   // heightn = (*height) * mult;
-   // file_sizen = ((widn) * (heightn) * 3) + 54;
-
-   // PIXELDATA** pixels_n = new PIXELDATA * [heightn];
-   // for (int i = 0; i < heightn; i++)
-   // {
-   //     pixels_n[i] = new PIXELDATA[widn];
-   // }
-   // //PIXELDATA pixels_n[16][16];
-
-   // int w = 0, h = 0;
-
-   // w = *wid;
-   // h = *height;
-   // for (int i = 0; i < heightn; i++)
-   // {
-   //     for (int j = 0; j < widn; j++)
-   //     {
-   //         pixels_n[i][j].blueC = pixels[(i * h) / heightn][(j * w) / widn].blueC;
-   //         pixels_n[i][j].greenC = pixels[(i * h) / heightn][(j * w) / widn].greenC;
-   //         pixels_n[i][j].redC = pixels[(i * h) / heightn][(j * w) / widn].redC;
-   //     }
-   // }
-
-   // /*for (int i = 0; i < *wid; i++)
-   // {
-   //     for (int j = 0; j < *height; j++)
-   //     {
-   //         pixels_n[i_n][j_n].blueC = pixels[(i * (*wid)) / widn][(j * (*height)) / heightn].blueC;
-   //         pixels_n[i][j_n].greenC = pixels[(i * (*wid)) / widn][(j * (*height)) / heightn].greenC;
-   //         pixels_n[i*mult][j].redC = pixels[(i * (*wid)) / widn][(j * (*height)) / heightn].redC;
-   //     }
-   // }*/
-
-   // std::fstream pfile("lab4r.bmp", std::ios::out | std::ios::binary);
-   // //FILE* pfile = fopen("lab4r.bmp", "wb");
-   // //std::cout << sizeof(int) << std::endl;
-   // /*unsigned char* WID = (unsigned char*)&widn;
-   // unsigned char* HEI = (unsigned char*)&heightn;*/
-   // int* WID;
-   // int* HEI;
-
-   // 
-   // padding = (4 - (widn * 3) % 4) % 4;
-   // int FILE_LEN = (widn * heightn * 3) + 54 + padding * heightn;
-   // int* len1;
-   // len1 = (int*)&HEAD[2];
-   // *len1 = FILE_LEN;
-   // len1 = (int*)&HEAD[18];
-   // *len1 = widn;
-   // len1 = (int*)&HEAD[22];
-   // *len1 = heightn;
-   // //HEAD[2] = 0xC0;
-   // //HEAD[3] = 0x36;
-   // //HEAD[18] = 0x80;
-   // //HEAD[22] = 0x80;
-   // /*HEAD[18] = WID[0];
-   // HEAD[19] = WID[1];
-   // HEAD[20] = WID[2];
-   // HEAD[21] = WID[3];
-   // HEAD[22] = HEI[0];
-   // HEAD[23] = HEI[1];
-   // HEAD[24] = HEI[2];
-   // HEAD[24] = HEI[3];*/
-
-   // //std::cout << file_sizen;
-   // //pfile.write((char*)&HEAD, 54);
-   // for (int i = 0; i < 54; i++)
-   // {
-   //     pfile << HEAD[i];
-   // }
-   // //fwrite((unsigned char*)&HEAD, 1, 54, pfile);
-   // //std::cout << std::endl;
-
-   //// unsigned char* all_pixels = new unsigned char[file_sizen - 54];
-   //// //unsigned char all_pixels[768];
-
-   //// for (int i = 0; i < widn; i++)
-   //// {
-   ////     for (int j = 0; j < heightn; j++)
-   ////     {
-   ////         pfile << pixels_n[i][j].blueC;
-   ////         pfile << pixels_n[i][j].greenC;
-   ////         pfile << pixels_n[i][j].redC;
-   ////     }
-   //// }
-   //// int count = 0;
-   //// for (int i = 0; i < widn; i++)
-   //// {
-   ////     for (int j = 0; j < heightn; j++)
-   ////     {
-   ////         pointer = i * (widn)*3 +j * 3;
-   ////         all_pixels[pointer] = pixels_n[i][j].blueC;
-   ////         all_pixels[pointer+1] = pixels_n[i][j].greenC;
-   ////         all_pixels[pointer+2] = pixels_n[i][j].redC;
-   ////         /*all_pixels[pointer] = 0x00;
-   ////         all_pixels[pointer + 1] = 0x00;
-   ////         all_pixels[pointer + 2] = 0x00;
-   ////         count++;*/
-   ////     }
-   //// }
-   //// /*std::cout << std::endl;
-   //// std::cout << pointer;
-   //// std::cout << std::endl;
-   //// std::cout << count;*/
-   /////* for (int i = 0; i < 768; i++)
-   //// {
-   ////     std::cout << all_pixels[i] << std::endl;
-   //// }*/
-   //// for (int i = 0; i < 54; i++)
-   //// {
-   ////     std::cout << i+1 << " " << HEAD[i] << std::endl;
-   //// }
-   //
-   //for (int i = 0; i < heightn; i++)
-   //{
-   //     for (int j = 0; j < widn; j++)
-   //     {
-   //         pfile << pixels_n[i][j].blueC;
-   //         pfile << pixels_n[i][j].greenC;
-   //         pfile << pixels_n[i][j].redC;
-   //     }
-   //     if (padding != 0)
-   //     {
-   //         pfile.write("\0", padding);
-   //     }
-   //}
-   //// //pfile.write((char*)all_pixels, file_sizen - 54);
-   //pfile.close();
-    /*std::string name;
-    getline(std::cin, name);*/
-
-    BITMAP input_bmp(_argv[1]);
-    BITMAP output_bmp(input_bmp, atoi(_argv[3]));
-    output_bmp.OUTPUT_BITMAP(_argv[2]);
-    }
+    BITMAP input_bmp(_argv[1]); //отримуємо файл
+    BITMAP output_bmp(input_bmp, atoi(_argv[3])); //отримуємо файл результату
+    output_bmp.OUTPUT_BITMAP(_argv[2]); //виводимо файл
+}
